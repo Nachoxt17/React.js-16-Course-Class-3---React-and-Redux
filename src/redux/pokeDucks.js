c0a1ba172472f6d2a05384222159f498736870f9
Nachoxt17 <nacho.man.kpo@hotmail.com>
2020-10-01 19:30:39 -0300

    Second Commit

--- src/redux/pokeDucks.js
+++ src/redux/pokeDucks.js
@@ -10,6 +10,10 @@ const GET_POKEMONS_SUCCESS = 'GET_POKEMONS_SUCCESS';
 // +-Reducers:_
 export default function pokeReducer(state = dataInicial, action) {
     switch (action.type) {
+        case GET_POKEMONS_SUCCESS:
+            return { ...state, array: action.payload };
+        default:
+            return state;
     }
 }
 
@@ -17,4 +21,16 @@ export default function pokeReducer(state = dataInicial, action) {
 /**+-A continuación se van a Utilizar 2 Funciones de Flecha Porque en la 1ra Función vamos a recibir los Parámetros que se van a enviar a "getPokemonsAction" y en la
  * 2da Función vamos a recibir los Parámetros "dispatch" para activar el Reducer "pokeReducer" y "getState" para obtener la Información de "dataInicial".
  * +-Al igual que cada vez que se llama a una API, vamos a usar async-await y además vamos a usar "axios".*/
-export const getPokemonsAction = () => (dispatch, getState) => {};
+export const getPokemonsAction = () => async (dispatch, getState) => {
+    try {
+        const res = await axios.get(
+            'https://pokeapi.co/api/v2/pokemon?offset=0&limit=20'
+        );
+        dispatch({
+            type: GET_POKEMONS_SUCCESS,
+            payload: res.data.results,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
