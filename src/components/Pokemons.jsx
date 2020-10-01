28dbe28130cc0af8d89b22c82f9735fd8c63899a
Nachoxt17 <nacho.man.kpo@hotmail.com>
2020-10-01 20:12:02 -0300

    Third Commit

---
+++ src/components/Pokemons.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+/**+-"useDisptach" sirve para consumir las Actions de "pokeDucks" y "useSelector" para acceder al State Inicial(dataInicial) también de "pokeDucks".*/
+
+import { getPokemonsAction } from '../redux/pokeDucks';
+
+const Pokemons = () => {
+    const dispatch = useDispatch();
+
+    const pokemons = useSelector((store) => store.pokemons.array);
+
+    return (
+        <div>
+            Pokemons List
+            <button onClick={() => dispatch(getPokemonsAction())}>
+                Get Pokemons
+            </button>
+            <ul>
+                {pokemons.map((item) => (
+                    <li key={item.name}>{item.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Pokemons;
